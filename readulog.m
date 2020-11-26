ccb9d87c87f5d9a42d70b6eb0ae456732395a653
xiaowei <zhaoxiaowei@efy-tech.com>
2020-11-26 14:54:02 +0800

    add plot

--- readulog.m
+++ readulog.m
@@ -1,4 +1,5 @@
 %% run open log file 
+clear;
 handles.current_fileName = 'xxxx';
 handles.current_dir_PathName = 'xxxx';
 [handles.current_fileName, handles.current_dir_PathName] = get_current_logfile_popup(handles);
@@ -14,18 +15,37 @@ loggedoutput = readLoggedOutput(ulog);
 log = readLoggedOutput(ulog,'Time',[d1 d2]);
 
 %% 获取tuc时间
-d_s= d1 + duration([0 0 55],'Format','hh:mm:ss.SSSSSS');
+d_s= d1 + duration([0 1 55],'Format','hh:mm:ss.SSSSSS');
 dance_utc = readTopicMsgs(ulog,'TopicNames',{'dance_utc_log'}, 'InstanceID',{0},'Time',[d_s d2]);
 utc_log = dance_utc.TopicMessages{1,1};
 figure;
 plot(utc_log.timestamp,utc_log.pps_time_utc_usec,'o');hold on;
 plot(utc_log.timestamp,utc_log.gps_time_utc_usec,'*');
+legend('pps time','gps time');
+grid;
+title('pps校准时间对比');
 %% dance log 
 dance_log = readTopicMsgs(ulog,'TopicNames',{'dance_step_log'}, 'InstanceID',{0},'Time',[d1 d2]);
 dance_position = dance_log.TopicMessages{1,1};
 
 dance_step_position = readTopicMsgs(ulog,'TopicNames',{'dance_step_position'}, 'InstanceID',{0},'Time',[d1 d2]);
 dance_position_receive = dance_step_position.TopicMessages{1,1};
+figure;
+subplot(3,1,1);
+plot(dance_position_receive.timestamp,dance_position_receive.step_cnt);
+legend('receive dance step');
+grid;
+title('接收航点cnt');
+subplot(3,1,2);
+plot(dance_position.timestamp,dance_position.step_cnt);
+legend('send dance step');
+grid;
+title('舞步发送cnt');
+subplot(3,1,3);
+plot(dance_position.timestamp,dance_position.unused_fifo_size);
+legend('unused_fifo_size');
+grid;
+title('fifo使用情况');
 %% 获取姿态角
 vehicle_attitude_data = readTopicMsgs(ulog,'TopicNames',{'vehicle_attitude'}, ... 
  'InstanceID',{0},'Time',[d1 d2]);
